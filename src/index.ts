ae85fb6eed9c181421ecf2719b53c33b0f9598ec
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-01 19:28:18 -0300

    feat: add CreateUserController with validations

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import express from "express";
 import { config } from "dotenv";
+import express from "express";
+import { CreateUserController } from "./controllers/create-user/create-user";
 import { GetUsersController } from "./controllers/get-users/get-users";
-import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { MongoClient } from "./database/mongo";
+import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-user";
+import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 
 const main = async () => {
   config();
 
   const app = express();
+
+  app.use(express.json());
 
   await MongoClient.connect();
 
@@ -17,7 +21,20 @@ const main = async () => {
 
     const { body, statusCode } = await getUsersController.handle();
 
-    res.send(body).status(statusCode);
+    res.status(statusCode).send(body);
+  });
+
+  app.post("/users", async (req, res) => {
+    const mongoCreateUserRepository = new MongoCreateUserRepository();
+    const createUserController = new CreateUserController(
+      mongoCreateUserRepository
+    );
+
+    const { body, statusCode } = await createUserController.handle({
+      body: req.body,
+    });
+
+    res.status(statusCode).send(body);
   });
 
   const port = process.env.PORT || 8000;
